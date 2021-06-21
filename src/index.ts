d806b1b3bd10432183dc355cb2e856e7ef72fc24
Franco <fran.e.zarate@gmail.com>
2021-06-21 20:29:22 -0300

    Ejercicio 9 finalizado. Aplicado el formulario para cargar productos

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,35 @@ import express from 'express'
 import Item from './items'
 import { getById } from './functions';
 import router from './itemsRouter'
+import { string } from 'prop-types';
+let path = require('path')
 const app = express();
-
 app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
-app.use('/', router)
+//MIDDLEWARE 
+
+/*app.use(function(req,res,next){//MIDDLEWARE DE APLICACION ocurre antes de que se procese la ruta. ESto es global, para cada ruta en especifico ver el siguiente
+    console.log('Soy una etapa anterior');
+    next();
+})*/
+
+app.use('/items',function(req,res,next){//MIDDLEWARE con ruta especifica
+    
+    next();
+})
+
+app.get('/index',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../public','/index.html'))
+})
+
+app.get('/', (req,res)=>{
+    res.send("Ingresar a /index para cargar productos  \nIngresar a /items para ver los productos cargados ")
+})
+
+app.use('/items', router)
+app.use('/',router)
+app.use(express.static('public'))
 
 const server = app.listen(8080, ()=>{
     console.log("Running on port 8080");
