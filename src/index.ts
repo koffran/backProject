ad07c3c43af9f0182ff0cd7fcd9114cf7113ef82
Franco Zarate <fran.e.zarate@gmail.com>
2021-06-24 01:02:47 -0300

    Inicializada y creada la base de socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,52 @@
-import express from 'express'
+import express,{Request,Response} from 'express'
 import Item from './items'
 import { getById } from './functions';
 import router from './itemsRouter'
-import { string } from 'prop-types';
+import { NextFunction } from 'express-serve-static-core';
+import { Socket } from 'socket.io';
+//import { string } from 'prop-types';
 let path = require('path')
-const app = express();
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
-//MIDDLEWARE 
 
-/*app.use(function(req,res,next){//MIDDLEWARE DE APLICACION ocurre antes de que se procese la ruta. ESto es global, para cada ruta en especifico ver el siguiente
-    console.log('Soy una etapa anterior');
-    next();
-})*/
+io.on('connection', (socket:Socket)=>{
+    console.log(socket.id);
+    socket.emit('prueba',"mensaje de prueba")
 
-app.use('/items',function(req,res,next){//MIDDLEWARE con ruta especifica
-    
-    next();
+    socket.on('product created', (data:any)=>{
+        console.log(data)
+    })
 })
 
-app.get('/index',(req,res)=>{
+
+
+
+
+
+
+app.get('/index',(req:Request,res:Response)=>{
     res.sendFile(path.join(__dirname,'../public','/index.html'))
 })
 
-app.get('/', (req,res)=>{
+app.get('/', (req:Request,res:Response)=>{
     res.send("Ingresar a /index para cargar productos  \nIngresar a /items para ver los productos cargados ")
 })
 
+
+app.use('/items',function(req:Request,res:Response,next:NextFunction){//MIDDLEWARE con ruta especifica
+    
+    next();
+})
 app.use('/items', router)
 app.use('/',router)
 app.use(express.static('public'))
 
-const server = app.listen(8080, ()=>{
+const server = http.listen(8080, ()=>{
     console.log("Running on port 8080");
 })
-server.on("error", error => console.log(`Error en el servidor ${error}`))
+server.on("error", (error: any) => console.log(`Error en el servidor ${error}`))
